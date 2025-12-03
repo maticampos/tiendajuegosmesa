c358b59fc630d13476ecd276c6ac58d09d20f935
Matías Ignacio Campos <89554561+maticampos@users.noreply.github.com>
2025-12-02 23:28:01 -0300

    Add files via upload

---
+++ pages/Inicio.jsx
@@ -0,0 +1,32 @@
+import React from 'react'
+
+function Inicio() {
+  return (
+    <>
+    <div style={{textAlign: 'center'}}>
+    <h1>| Juegos de Mesa |  </h1>
+    <hr />
+    <main>
+      <p>El <strong>Senet</strong> fue un juego muy popular en el antiguo Egipto.</p> <p>Además del entretenimiento, poseía un profundo sentido ritual vinculado a la vida eterna. Los egipcios creían que desarrollar habilidades estratégicas para controlar el azar durante el juego era una forma de prepararse para justificar sus acciones en la vida y ante los dioses.</p>
+    </main>
+
+    <img src="https://www.glueckshaendler.de/wp-content/uploads/2020/09/Senet-Brettspiel-aus-Holz-mit-Spielsteinen.jpg"></img>
+    <br />
+        <img src="https://blogger.googleusercontent.com/img/b/R29vZ2xl/AVvXsEi1jKo9c_Vh2Rsg0jUj4idDOhBge3smcm8qrjmJklxoNe9vPpFSKJWhfi00lMaGrpn3oyAABwhhVwrxxnDk0Vsvno-HiGhOuUXN0mC_42I8CLswGKJzUHgl-UqtkDdEf5SsbmsAHg9CxZ0/s320/492ca122590140b5b1366be3336cd2d3.jpg" width="20%"></img>
+    <br />
+    <a href="https://www.man.es/man/en/educacion/recursos/juegos.html" target="_blank">[+] info</a>
+    <br />
+    <hr />
+    <a href="https://play.google.com/store/apps/details?id=air.EgyptianSenetFree&hl=es">Android</a>
+    <br />
+    <a href="https://apps.apple.com/us/app/egyptian-senet/id598710611">IOS</a>
+    <br />
+
+    <br />
+    <a href="https://www.danielschavelzon.com.ar/?p=2927" target='_blank'>Juegos en Tiempos de la Colonia</a>
+    </div>
+    </>
+  )
+}
+
+export default Inicio
